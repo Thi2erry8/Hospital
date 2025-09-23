91057fef88d33bd9da9e85e79fcf12b5dbf9301e
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-09-23 00:16:25 -0400

    header_start

--- hospital/src/copoments/header.jsx
+++ hospital/src/copoments/header.jsx
@@ -1,8 +1,22 @@
+import logo from "../assets/images/logo.png";
+
 function Header(){
    
     return(
         <>
-           <header className="w-full h-[70px] fixed bg-red-500">         
+           <header className="w-full bg-amber-50 h-[70px] fixed px-4 py-1 z-10 flex flex-row items-center justify-between">  
+                    <div className="h-full"> 
+                                     <img src={logo} className="h-full" alt="" />
+                    </div>  
+                    <nav className="flex flex-row items-center justify-between">
+                          <p>Nos services</p>
+                          <p>A propos de nous</p>
+                          <p></p>
+                          <p>Prendre Rendez-vous</p>
+                    </nav>    
+                    <div className=" h-full text-slate-500">
+                          <i className="ri-menu-line"></i>
+                    </div> 
            </header>
         </>
     )
