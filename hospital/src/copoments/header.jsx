ff6660a79819bd057f8b78919826dc9b66f6d75a
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-09-25 22:56:41 -0400

    header_end

--- hospital/src/copoments/header.jsx
+++ hospital/src/copoments/header.jsx
@@ -11,13 +11,13 @@ function Header(){
                     <div className="h-full"> 
                                      <img src={logo} className="h-full" alt="" />
                     </div>  
-                    <nav className="flex flex-row items-center justify-between gap-4">
+                    <nav className={`flex flex-col ${show ?"scale-y-100" :"scale-y-0"} origin-top duration-200 items-start justify-between gap-4 fixed bg-white top-19`}>
                           <p className="text-lg text-sky-900 cursor-pointer font-semibold hover:text-sky-500 transition duration-700 ease-in-out">Home</p>
                           <button className="text-lg text-sky-900 cursor-pointer font-semibold  focus:text-sky-500 transition duration-700 ease-in-out group relative"
                           onClick={() => setOpen(!open)}>Nos services 
                               <i className={`inline-block ${ open ? "transform rotate-180 " : " "}  duration-500 ease-in-out ri-arrow-down-s-fill`}></i>
-                              <ul className={`w-3xs items-start flex flex-col absolute top-full left-0 p-3 mt-1 shadow-md 
-                              ${open ? "scale-y-100" : "scale-y-0"} origin-top duration-200`}>
+                              <ul className={`w-3xs bg-sky-50 items-start flex flex-col absolute top-full left-0 p-3 mt-1 shadow-md 
+                              ${open ? "scale-y-100" : "scale-y-0 hidden"} origin-top duration-200`}>
                                     <li className="hover:text-sky-500"><a href="">Sante de la femme</a></li>
                                     <li className="hover:text-sky-500"><a href="">Sante mental</a></li>
                                     <li className="hover:text-sky-500"><a href="">Home</a></li> 
@@ -26,9 +26,10 @@ function Header(){
                           <p className="text-lg text-sky-900 cursor-pointer font-semibold hover:text-sky-500 transition duration-700 ease-in-out">A propos de nous</p>
                           <p className="text-lg text-sky-900 cursor-pointer font-semibold hover:text-sky-500 transition duration-700 ease-in-out">Prendre Rendez-vous</p>
                     </nav>    
-                    <div className="flex items-center h-full text-2xl text-sky-500">
-                          <i className="ri-menu-line"></i>
-                    </div> 
+                    <button className="flex items-center h-full text-2xl text-sky-500"
+                    onClick={() => setShow(!show)}>
+                          <i className={` ${show ?"ri-close-line":"ri-menu-line"} `}></i>
+                    </button> 
            </header>
         </>
     )
