f436bfe0bef15ef2d8cd0d4838081164af540355
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-09-26 20:46:22 -0400

    hero_start

--- hospital/src/pages/home.jsx
+++ hospital/src/pages/home.jsx
@@ -1,8 +1,23 @@
 import Header from "../copoments/header";
+import pic1 from "../assets/images/header_pic.jpg";
 
 function Home(){
      return(<>
           <Header/>
+          
+          <main className="pt-[70px]">
+                 {/* hero */}
+                 <section className="relative w-full min-h-screen flex flex-col items-center">
+                           <div className="absolute w-full h-full hidden">
+                                 <img className="" src={pic1} alt="" />
+                           </div>
+                           <div className="">
+                                 <p className=""></p>
+                                 <p className=""></p>
+                           </div>
+
+                 </section>
+          </main>
      </>)  
 }
 export default Home
