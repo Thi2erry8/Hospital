fbddeabe28d8a7789a37262ca71ae3a4b880b4a4
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-09-28 22:14:31 -0400

    services_end

--- hospital/src/pages/home.jsx
+++ hospital/src/pages/home.jsx
@@ -1,19 +1,27 @@
 import Header from "../copoments/header";
 import pic1 from "../assets/images/header_pic.jpg";
+import ser1 from "../assets/images/doctor.svg";
+import ser2 from "../assets/images/ambulance.svg";
+import ser3 from "../assets/images/partage.svg";
 
 function Home(){
 
      const services =[
-          {icon:"",
-           texte_1:"",
-           texte_2:""
+          {id:1,
+           icon:ser1,
+           texte_1:"Des soins professionnels",
+           texte_2:"Les clients de LOTUS bénéficient de l’expertise de plusieurs professionnels de la santé"
           },
-          {icon:"",
-           texte_1:"",
-           texte_2:""},
-          {icon:"",
-           texte_1:"",
-           texte_2:""}
+          {id:2,
+           icon:ser3,
+           texte_1:"Un soutien personnalisé.",
+           texte_2:"Grâce à notre technologie, nous aidons à transformer l’expérience des soins de santé."
+          },
+          {id:3,
+           icon:ser2,
+           texte_1:"Un accès rapide",
+           texte_2:"Notre réseau de cliniques met l’accent sur des soins adaptés à chaque patient."
+          }
      ]
 
      return(<>
@@ -36,8 +44,19 @@ function Home(){
                            </div>
 
                  </section>
-                 <section className="">
-
+                 <section className="w-full min-h-screen flex flex-col gap-12 items-center justify-center">
+                            { services.map((service) =>(
+                              <div key={service.id} className="flex flex-row h-1/4 w-8/10 gap-2 shadow-lg shadow-sky-200 p-2 rounded">
+                                    <img className="h-[60px]" src={service.icon} alt="service logo" />
+                                    <div className="flex flex-col gap-2 text-sky-900">
+                                          <p className="font-semibold">{service.texte_1}</p>
+                                          <p className="">{service.texte_2}</p>
+                                    </div>
+                              </div>
+                            ))}
+                 </section>
+                 <section className="w-full min-h-screen ">
+                    
                  </section>
           </main>
      </>)  
