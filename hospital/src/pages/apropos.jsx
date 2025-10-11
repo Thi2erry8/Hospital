d151fcad7cef641d9794beb42a2606845aa07ebd
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-10-11 00:32:19 -0400

    Aprpos_start

---
+++ hospital/src/pages/apropos.jsx
@@ -0,0 +1,15 @@
+import Header from "../copoments/header";
+import Footer from "../copoments/footer";
+
+export default function Apropos() {
+  return (<>
+    <Header></Header>
+
+    <main>
+           
+    </main>
+
+    <Footer></Footer>
+    </>
+  )
+}
