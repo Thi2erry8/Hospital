67cf0a1163374e269c2761509654c8657adfbbcc
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-10-10 22:23:02 -0400

    footer_start

--- hospital/src/copoments/footer.jsx
+++ hospital/src/copoments/footer.jsx
@@ -1,4 +1,12 @@
+import logo from  "../assets/images/logo.png";
+
 function footer(){
-       
+       return(
+         <footer className="flex flex-col">
+                  <div className="">
+                         <img src="" alt="" />
+                  </div>
+         </footer>
+       )
 } 
 export default footer
