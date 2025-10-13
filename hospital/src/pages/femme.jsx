ad71889e63252eab380dc9bce1b147417def7649
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-10-13 19:49:13 -0400

    01010

---
+++ hospital/src/pages/femme.jsx
@@ -0,0 +1,18 @@
+import Header from "../copoments/header";
+import Footer from "../copoments/footer";
+
+export default function femme() {
+  return (
+    <main className=" pt-[70px]">
+            <section className="min-h-screen">
+                      <div className="">
+                        
+                      </div>
+
+                      <div className="">
+
+                      </div>
+            </section>
+    </main>
+  )
+}
