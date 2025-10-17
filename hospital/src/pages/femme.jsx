d7bdc3f2402710bc132055091ad19f6358685760
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-10-16 21:31:14 -0400

    0101001

--- hospital/src/pages/femme.jsx
+++ hospital/src/pages/femme.jsx
@@ -4,9 +4,23 @@ import Footer from "../copoments/footer";
 export default function femme() {
   return (
     <main className=" pt-[70px]">
-            <section className="min-h-screen">
-                      <div className="">
-                        
+            <section className="min-h-screen w-screen flex flex-col">
+                      <div className="flex flex-col w-full">
+                             <p>
+                                 À la Clinique de santé M, nous offrons une gamme complète de services en santé de la femme, 
+                                 assurés par nos médecins de famille et infirmières praticiennes spécialisées (IPS). Que ce soit pour un 
+                                 suivi gynécologique, une contraception personnalisée ou des examens préventifs
+                                 , notre équipe vous accompagne avec écoute, respect et professionnalisme.
+                             </p>
+
+                             <p>
+                                 Nous proposons des consultations accessibles et confidentielles pour les femmes de tous âges,
+                                 dans un environnement sécuritaire et sans jugement.
+                             </p>
+
+                             <p>
+                                 Consultez la section tarifs pour plus de détails sur nos services en santé féminine.
+                             </p>
                       </div>
 
                       <div className="">
